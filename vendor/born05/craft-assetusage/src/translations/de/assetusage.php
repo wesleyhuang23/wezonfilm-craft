26619b04244e0c202fec6af338ca80bd916370eb
wesley-gdd <wesley@good.dog>
2020-05-02 14:00:55 -0700

    reset git cache

--- vendor/born05/craft-assetusage/src/translations/de/assetusage.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'Asset Usage plugin loaded' => 'Asset Usage plugin wurde geladen.',
-    'Usage' => 'Verwendung',
-    'Used {count} time' => '{count}x verwendet',
-    'Used {count} times' => '{count}x verwendet',
-    'Unused' => 'Ungenutzt',
-];
